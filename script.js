aba2e83b06854580d719c442d4f819eed42ba4b9
Koaua <125333005+Koaua@users.noreply.github.com>
2025-01-24 17:18:11 -1000

    Adding Stream page

---
+++ script.js
@@ -0,0 +1,8 @@
+function login() {
+    const username = document.getElementById('login_username').value;
+    const password = document.getElementById('login_password').value;
+
+    if (username == 'media' && password == 'media') {
+        document.
+    }
+}
